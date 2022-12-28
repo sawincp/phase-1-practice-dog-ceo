ab433381a29064c45ecdabb1f2dab25f99b9a3a3
Corey Sawin <sawincp@gmail.com>
2022-12-28 11:33:52 -0500

    update img and breeds

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+function getImg(){
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(data => data.message.forEach(data => renderImg(data)))
+}
+
+function getBreeds(){
+    fetch(breedUrl)
+    .then(response => response.json())
+    .then(data => Object.keys(data.message).forEach(data => renderBreeds(data)))
+    
+}
+
+function renderImg(data){
+    let img = document.createElement('img')
+    img.className = 'images'
+    img.src =`${data}`
+    document.getElementById('dog-image-container').appendChild(img)
+}
+
+function renderBreeds(data){
+    let li = document.createElement('li')
+    li.innerHTML = `${data}`
+    document.getElementById('dog-breeds').appendChild(li)
+    
+}
+
+
+function intialize(){
+    getImg()
+    getBreeds()
+}
+intialize()
